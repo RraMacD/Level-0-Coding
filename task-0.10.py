108a7698a9bd36a29c3bc03f1689bb50f2fc9c00
RraMacD <74976917+RraMacD@users.noreply.github.com>
2021-04-30 11:02:12 +0200

    Update task-0.10.py
    Reworked to requirements

--- task-0.10.py
+++ task-0.10.py
@@ -1,6 +1,7 @@
-user_1 =input('Add a word: ')
-user_2 =input('Add a word: ')
-word_1 = set ( user_1)
-word_2 = set (user_2)
-list = [word_1 & word_2]
-print('common letters: {}' .format(list))
+def compare_words(a,b):
+    word_1 = set(a)
+    word_2 = set(b)
+    list = [word_1 & word_2]
+    return list
+print(compare_words(input(),input()))
+
