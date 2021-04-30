6c08896062a92c236c3d46bd8b25a1348dce2f77
RraMacD <74976917+RraMacD@users.noreply.github.com>
2021-04-30 10:41:48 +0200

    Update task-0.9.py
    Reworked to requirements

--- task-0.9.py
+++ task-0.9.py
@@ -1,28 +1,11 @@
-word = input("Please enter a word: ")
-if 'a' in word or 'e' in word or 'i' in word or 'o' in word or 'u' in word:
-    print("vowel found in the word: " + word)
-elif 'A' in word or 'E' in word or 'I' in word or "O" in word or 'U' in word:
-    print ("vowel found: " + word)
-else:
-    print("There are no vowels in the word")
 
-if 'a' in word:
-    print('a')
-if 'A' in word:
-    print('A')
-if 'e' in word:
-    print('e')
-if 'E' in word:
-    print('E')
-if 'i' in word:
-    print('i')
-if 'I' in word:
-    print('I')
-if 'o' in word:
-    print('o')
-if 'O' in word:
-    print('O')
-if 'u' in word:
-    print('u')
-if 'U' in word:
-    print('U')
+def find_vowels(string):
+    vowels_list = "aeiouAEIOU"
+    res = set([each for each in string if each in vowels_list])
+    return res
+print("vowels found in word are: ", find_vowels(input()))
+    
+
+
+
+
