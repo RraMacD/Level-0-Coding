f39bf806cfba0c4aa21ede534d44500108d3fe26
RraMacD <74976917+RraMacD@users.noreply.github.com>
2021-05-11 12:06:00 +0200

    Update task-0.8.py
    Reworked to requirements

--- task-0.8.py
+++ task-0.8.py
@@ -1,9 +1,8 @@
-def time_convert(a):
+def time_convert(a): 
     hour = a // 60
-    return hour 
-print('Hour is: ',time_convert(int(input())))
+    minute = a % 60
+    return hour, minute
+print('Hour and minute of this number is: ',time_convert(int(input()))) 
 
-def time_convert_minute(b):
-    minute = b % 60
-    return minute
-print('Minute is: ',time_convert(int(input())))
+
+
