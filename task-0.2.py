add4d6d14053f3dd977aa0296d1ece484f90fa9d
RraMacD <74976917+RraMacD@users.noreply.github.com>
2021-10-01 09:22:24 +0200

    Update task-0.2.py

--- task-0.2.py
+++ task-0.2.py
@@ -1,19 +1,7 @@
 X = 1 + 1 * 2
-
-Y = (1+1) *2
-
-Z = 1 +(1 *2)
-
-A = 1 + 1 *2 / 2
-
+Y = (1 + 1) * 2
+Z = 1 + (1 * 2)
+A = 1 + 1 * 2 / 2
 B = (1 + 1 * 2) / 2
-
-print (X)
-
-print (Y)
-
-print (Z)
-
-print (A)
-
-print (B)
+sum_of_letters = X + Y + Z + A + B
+print(sum_of_letters)
