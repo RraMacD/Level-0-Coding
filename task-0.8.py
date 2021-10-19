6069e2d1c7fb3da25f9d97aa49520e2260709f6a
RraMacD <74976917+RraMacD@users.noreply.github.com>
2021-10-19 12:49:37 +0200

    Update task-0.8.py

--- task-0.8.py
+++ task-0.8.py
@@ -1,6 +1,8 @@
-num = int(input('Enter any number: ')) 
-hour = round(num / 60) 
-minute = num % 60
-time1 = "hours"
-time2 = "minutes in this number"
-print ('There are: {0}'.format(hour) + " " + time1, '{0}'.format(minute) + " " + time2)
+def  timeConversion(x):
+  hour =  round(x  /  60 
+  minute  =  x  %  60 
+  time1  =  "hours" ) 
+  time2  =  "minutes  in this  number" 
+  print  ('There  are:  {0}'.format(hour)  +  "  "  +  time1, '{0}'.format(minute)  +  "  "  +  time2) 
+
+timeConversion(200)
