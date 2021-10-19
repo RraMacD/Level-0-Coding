e298dfac289876b68b761c4e67dadf5104a694a1
RraMacD <74976917+RraMacD@users.noreply.github.com>
2021-10-19 12:45:16 +0200

    Update task-0.7.py

--- task-0.7.py
+++ task-0.7.py
@@ -1,10 +1,14 @@
 #Formula for degree celcius to fahrenheit celcius = (user_fahrenheit -32) * 5/9 
 #Formula for fahrenheit to degree celcius fahrenheit = (user_celcius * 9/5) + 32 
 
-user_number = float(input('Enter a number in celcius degree: '))
-celciusDegree_to_fahrenheit = (user_number - 32)* 5/9
-print('Your temparature in Fahrenheit is: {0}' .format(celciusDegree_to_fahrenheit))
+def celciusDegree(x):
+  celciusDegree_to_fahrenheit = (x - 32)* 5/9
+  print('Your temparature in Fahrenheit is:',celciusDegree_to_fahrenheit)
 
-user_number2 = float(input('Enter a number in fahrenheit: '))
-fahrenheit_to_celciusDegree = (user_number2 * 9/5) + 32
-print('Your temparature in celcius degrees is: {0}' .format(fahrenheit_to_celciusDegree))
+celciusDegree(37)
+
+def  fahrenheit(x): 
+  fahrenheit_to_celciusDegree  =  (x  *  9/5)  +  32 
+  print('Your temparature  in  Calcius Degree  is:',fahrenheit_to_celciusDegree) 
+
+fahrenheit(17)
