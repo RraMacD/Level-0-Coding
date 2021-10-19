b6fa4dfed8c3db424e17f265373c062cb3c61ace
RraMacD <74976917+RraMacD@users.noreply.github.com>
2021-10-19 12:53:00 +0200

    Update task-0.9.py

--- task-0.9.py
+++ task-0.9.py
@@ -1,4 +1,6 @@
-user_word = input("Enter a word: ")
-for i in user_word:
-    if (i=='a' or i=='e' or i=='u' or i=='i' or i=='o' or i=='A' or i=='E' or i=='U' or i=='I'  or i=='O'):
-        print("Vowels: " ,i)
+def checkVowels(x):
+     for i in x:
+         if i in 'aeiouAEIOU':
+             print("Vowels: " ,i)
+
+checkVowels('Home') 
